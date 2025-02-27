ffc4d5f26a5937b0d6c06285ce479747eccf045b
Vinicius Teruel <vpteruel@gmail.com>
2025-02-26 20:43:57 -0500

    done

---
+++ final-exam/src/main/java/com/vinicius/Main.java
@@ -0,0 +1,20 @@
+package com.vinicius;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        list.add("apple");
+
+        list.add("banana");
+
+        list.add("cherry");
+
+        System.out.println(list.get(1));
+
+    }
+}
